7d6bfed0b0297352c70994d2f587234a96ac754d
mikelietz <codger+git@gmail.com>
2012-01-10 18:11:06 -0500

    Updating to use XML fields for guid and help.

--- silencer.plugin.php
+++ silencer.plugin.php
@@ -2,23 +2,6 @@
 class Silencer extends Plugin
 {
 	/**
-	 * Add update beacon support
-	 **/
-	public function action_update_check()
-	{
-	 	Update::add( 'Silencer', 'fdb7e18c-f883-4456-a55d-b8e5c8c9ffaa', $this->info->version );
-	}
-
-	/**
-	 * Add help text to plugin configuration page
-	 **/
-	public function help()
-	{
-		$help = _t( 'Once this is enabled, "Comments Allowed" will be deselected in the settings of all newly published posts and pages.' 		);
-		return $help;
-	}
-
-	/**
 	 **/
 	public function action_plugin_activation( $file )
 	{

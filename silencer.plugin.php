47f9e2a1b8fd6dc8d66c3ddd683727206e4f0455
mikelietz <codger+github@gmail.com>
2013-04-04 14:07:44 -0300

    Remove unused function.

--- silencer.plugin.php
+++ silencer.plugin.php
@@ -2,22 +2,12 @@
 class Silencer extends Plugin
 {
 	/**
-	 **/
-	public function action_plugin_activation( $file )
-	{
-		if(Plugins::id_from_file($file) == Plugins::id_from_file(__FILE__)) {
-			// for now, do nothing. Someday global postinfo changes may go here.
-		}
-	}
-
-	/**
 	 * Update the setting prior to displaying the form.
 	 **/
 	public function action_form_publish( $form, $post, $context )
 	{
 		$form->settings->comments_enabled->value = false;
 	}
-
 }
 
 ?>
